8ff6ab7fab067be07fd949f207c285db77bc0391
pnaskardev <pnaskardev@gmail.com>
2025-03-22 10:50:06 +0530

    Refactor application structure and update metadata; remove unused styles and components, add user data fetching and loading states.

---
+++ app/user/page.tsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+async function fetchData() {
+  const response = await axios.get(
+    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
+  );
+  return response.data;
+}
+
+export default async function User() {
+  const userData = await fetchData();
+
+  return (
+    <div className="flex flex-col justify-center h-screen">
+      <div className="flex justify-center">
+        <div className="border p-8 rounded">
+          <div> Name: {userData?.name}</div>
+          Email:{userData?.email}
+        </div>
+      </div>
+    </div>
+  );
+}
