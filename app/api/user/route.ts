17d31f75d267cd4b94b9e038d8ab812b10755efb
pnaskardev <pnaskardev@gmail.com>
2025-03-22 12:43:55 +0530

    Add user API routes and signup component; implement user data fetching and signup functionality

---
+++ app/api/user/route.ts
@@ -0,0 +1,13 @@
+export function GET() {
+  // some database logic
+  return Response.json({
+    email: "pnaskardev@gmail.com",
+    name: "Priyanshu",
+  });
+}
+
+export function POST() {
+  return Response.json({
+    
+  });
+}
